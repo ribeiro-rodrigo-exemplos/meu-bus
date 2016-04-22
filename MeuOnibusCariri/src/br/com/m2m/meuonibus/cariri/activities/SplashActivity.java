42fc954385a7385aba2e3c01f193c07f136f47e8
Marco Marques <marco.marques@m2msolutions.com.br>
2016-04-22 11:47:38 -0300

    Meu Onibus Cariri & Maracanau

---
+++ MeuOnibusCariri/src/br/com/m2m/meuonibus/cariri/activities/SplashActivity.java
@@ -0,0 +1,34 @@
+package br.com.m2m.meuonibus.cariri.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import br.com.m2m.meuonibus.cariri.R;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ * 
+ * @see SystemUiHider
+ */
+public class SplashActivity extends ActionBarActivity implements Runnable {
+
+	private static final int SHOW_SPLASH_MILLIS = 3000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_splash_cariri);
+		Handler handler = new Handler();
+		handler.postDelayed(this, SHOW_SPLASH_MILLIS);
+	}
+
+	@Override
+	public void run() {
+		startActivity(new Intent(SplashActivity.this, MainActivity.class));
+		overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+		finish();
+	}
+}
