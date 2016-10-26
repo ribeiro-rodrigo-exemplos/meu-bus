e892d9f0321f86468f881eca68e0d1ca7b544a34
Marco Marques <marco.marques@m2msolutions.com.br>
2016-10-26 18:37:32 -0200

    Novos Aplicativos MeuOnibusViacaoModelo Saoluiz

---
+++ MeuOnibusSaoLuiz/src/br/com/m2m/meuonibus/saoluiz/handlers/PontoRecargaHandler.java
@@ -0,0 +1,18 @@
+package br.com.m2m.meuonibus.saoluiz.handlers;
+
+import java.util.ArrayList;
+
+import android.os.Handler;
+import br.com.m2m.meuonibus.saoluiz.models.PontoRecarga;
+
+public class PontoRecargaHandler extends Handler {
+
+	public void setPontoRecarga(PontoRecarga ponto) {
+	}
+
+	public void setPontosRecarga(ArrayList<PontoRecarga> pontos) {
+	}
+
+	public void setErro(Throwable e) {
+	}
+}
