e01be0c1361ca67ace10bbb6e580697efa46fade
Flora Saramago <flora.saramago@gmail.com>
2017-01-13 15:28:43 -0200

    Remove unused features

--- MeuOnibusAssetur/src/br/com/m2m/meuonibus/assetur/activities/ListaLinhasActivity.java
+++ MeuOnibusAssetur/src/br/com/m2m/meuonibus/assetur/activities/ListaLinhasActivity.java
@@ -5,11 +5,8 @@ import android.support.v4.app.Fragment;
 import br.com.m2m.meuonibus.assetur.R;
 import br.com.m2m.meuonibus.assetur.fragments.ListaLinhasFragment;
 import br.com.m2m.meuonibuscommons.activities.base.BaseWithTitleActivity;
-import br.com.m2m.meuonibuscommons.models.PontoOnibus;
 
 public class ListaLinhasActivity extends BaseWithTitleActivity {
-
-	public PontoOnibus ponto;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +15,7 @@ public class ListaLinhasActivity extends BaseWithTitleActivity {
 		setContentView(R.layout.activity_lista_linhas);
 
 		if (savedInstanceState == null) {
-			Fragment fragment = ListaLinhasFragment.newInstance(ponto);
+			Fragment fragment = ListaLinhasFragment.newInstance();
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container_lista_linha, fragment).commit();
 		}
